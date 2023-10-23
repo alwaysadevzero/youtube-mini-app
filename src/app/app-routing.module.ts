60022d3447392055def58a0e65272b5cf41617f4
Anthony <alwaysadevzero@gmail.com>
2023-10-23 17:37:57 +0400

    feat(app): configure routing for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, type Routes } from '@angular/router'
+import { MainPageComponent } from './main-page/main-page.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'main' },
+  {
+    path: 'main',
+    title: 'Main',
+    component: MainPageComponent,
+  },
+  {
+    path: '**',
+    title: '404 - Not found',
+    loadComponent: () =>
+      import('./core/components/not-found/not-found.component').then(component => component.NotFoundComponent),
+  },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from '../core/components/header/header.component'
 import { MainPageComponent } from './main-page.component'
+import { CardComponent } from './card/card.component'
+import { CardsContainerComponent } from './cards-container/cards-container.component'
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, CardComponent, CardsContainerComponent],
   imports: [CommonModule, HeaderComponent],
   exports: [HeaderComponent],
 })

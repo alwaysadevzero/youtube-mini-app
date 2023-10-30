4f9582103fe542a3578db007a11345648458b319
Anthony <alwaysadevzero@gmail.com>
2023-10-31 01:50:46 +0400

    feat(main): add main-page component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild, type AfterViewInit } from '@angular/core'
+import { HeaderComponent } from '../core/components/header/header.component'
+import type { SortSetting } from '../shared/model/sort-setting.interface'
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {}
+export class MainPageComponent implements AfterViewInit {
+  @ViewChild(HeaderComponent) childComponent!: HeaderComponent
+
+  showCards = false
+
+  sortSetting: SortSetting = {
+    sortByViewCount: null,
+    sortByDate: null,
+    sortByTitle: '',
+  }
+
+  processSearchClick() {
+    if (!this.showCards) this.showCards = true
+  }
+
+  ngAfterViewInit() {
+    this.childComponent.settingsClick.subscribe((sortSetting: SortSetting) => {
+      this.sortSetting = sortSetting
+    })
+  }
+}
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core'
+import { TuiLazyLoadingModule, TuiIslandModule } from '@taiga-ui/kit'
+import { TuiSvgModule, TuiButtonModule } from '@taiga-ui/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from '../core/components/header/header.component'
 import { MainPageComponent } from './main-page.component'
 import { CardComponent } from './card/card.component'
 import { CardsContainerComponent } from './cards-container/cards-container.component'
+import { DateColorizerDirective } from '../shared/directives/date-colorizer/date-colorizer.directive'
+import { SortVideoByDatePipe } from './cards-container/pipe/sort-video-date.pipe'
+import { SortVideoByViewsPipe } from './cards-container/pipe/sort-video-views.pipe'
+import { FilterVideoByTitlePipe } from './cards-container/pipe/filter-video-title.pipe'
+import { ButtonTextComponent } from '../shared/component/button-text/button-text.component'
 
 @NgModule({
-  declarations: [MainPageComponent, CardComponent, CardsContainerComponent],
-  imports: [CommonModule, HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [
+    MainPageComponent,
+    CardComponent,
+    CardsContainerComponent,
+    DateColorizerDirective,
+    SortVideoByDatePipe,
+    SortVideoByViewsPipe,
+    FilterVideoByTitlePipe,
+  ],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    ButtonTextComponent,
+    TuiLazyLoadingModule,
+    TuiIslandModule,
+    TuiSvgModule,
+    TuiButtonModule,
+  ],
 })
 export class MainPageModule {}

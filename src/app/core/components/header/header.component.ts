9bf2b16e1f01ca0d3294a966f2a51ba8afdee26e
Anthony <alwaysadevzero@gmail.com>
2023-11-12 04:00:17 +0400

    feat(header): add support for search and auth services

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { Component, DestroyRef, type OnInit, inject } from '@angular/core'
 import { TuiActiveZoneModule } from '@taiga-ui/cdk'
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { TuiInputModule, TuiActionModule } from '@taiga-ui/kit'
 import { TuiTextfieldControllerModule, TuiButtonModule, TuiDropdownModule } from '@taiga-ui/core'
 import { Router, RouterModule } from '@angular/router'
 import { TuiAvatarModule, TuiFallbackSrcModule } from '@taiga-ui/experimental'
-import { SearchSettingsService } from '../../../youtube/services/search-settings.service'
+import { debounceTime, filter } from 'rxjs/operators'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { SearchSettingsComponent } from '../search-settings/search-settings.component'
 import { AuthService } from '../../services/auth/auth.service'
+import { YoutubeHttpService } from '../../services/video-http/youtube-http.service'
 
 @Component({
   selector: 'app-header',
@@ -31,18 +33,36 @@ import { AuthService } from '../../services/auth/auth.service'
   styleUrls: ['./header.component.scss'],
   standalone: true,
 })
-export class HeaderComponent {
-  private searchSettings = inject(SearchSettingsService)
-
+export class HeaderComponent implements OnInit {
   private authService = inject(AuthService)
+
+  private destroyRef = inject(DestroyRef)
+
+  private youtubeHttp = inject(YoutubeHttpService)
 
   private router = inject(Router)
 
+  userCredentials$ = this.authService.userCredentials$
+
   userName$ = this.authService.userName$
 
-  userIsAuthorized$ = this.authService.userIsAuthorized$
-
   isSearchSettingsMenuOpen = false
+
+  searchForm = new FormGroup({
+    searchInput: new FormControl('', { nonNullable: true }),
+  })
+
+  ngOnInit(): void {
+    this.searchForm.controls.searchInput.valueChanges
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        filter(str => str.length > 2),
+        debounceTime(500),
+      )
+      .subscribe(value => {
+        this.youtubeHttp.searchVideos(value)
+      })
+  }
 
   settingsOnClick(): void {
     this.isSearchSettingsMenuOpen = !this.isSearchSettingsMenuOpen
@@ -50,10 +70,6 @@ export class HeaderComponent {
 
   onActiveZone(active: boolean): void {
     this.isSearchSettingsMenuOpen = active && this.isSearchSettingsMenuOpen
-  }
-
-  searchClick() {
-    this.searchSettings.onSearchVideosEvent(true)
   }
 
   logout() {
@@ -64,8 +80,4 @@ export class HeaderComponent {
   login() {
     this.router.navigate(['login'])
   }
-
-  searchForm = new FormGroup({
-    searchInput: new FormControl(),
-  })
 }
